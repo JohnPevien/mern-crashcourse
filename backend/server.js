028931ba88d5661fc7d661c93d808f1cc3778fc2
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-05-01 15:43:12 +0800

    Add MongoDB connection and products route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
+
+connectDB();
 
 const app = express();
 
+app.get("/products", (req, res) => {});
+
 app.listen(9015, () => {
-    console.log("Server is running on port 9015");
+	console.log("Server is running on port 9015");
 });
-  
-  
