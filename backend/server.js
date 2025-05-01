0e975c2df3cbfd48f207077bd025b5e4a886bd5e
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-05-01 16:19:59 +0800

    Adds product deletion endpoint

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ app.use(express.json()); //allow to parse JSON data in the request body
 app.post("/api/products", async (req, res) => {
 	const product = req.body;
 	if (!product.name || !product.price || !product.image) {
-		return res.status(400).json({ success: false, message: "Missing required fields" });
+		return res
+			.status(400)
+			.json({ success: false, message: "Missing required fields" });
 	}
 
 	const newProduct = new Product(product);
@@ -28,6 +30,24 @@ app.post("/api/products", async (req, res) => {
 		res.status(500).json({
 			success: false,
 			message: "Error creating product",
+			error: error.message,
+		});
+	}
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({
+			success: true,
+			message: "Product deleted successfully",
+		});
+	} catch (error) {
+		console.log("Error deleting product: ", error);
+		res.status(404).json({
+			success: false,
+			message: "Product not found",
 			error: error.message,
 		});
 	}
