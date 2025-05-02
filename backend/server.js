b6e1bc7db8bc25e9b128674bd998600c14d58b6d
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-05-02 23:47:46 +0800

    Configure server port and database connection timing
    - Moves database connection initialization to server startup
    - Use .env PORT and a fallback value

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
-connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 9015;
 
 app.use(express.json()); //allow to parse JSON data in the request body
 
 app.use("/api/products", productRouter);
 
-app.listen(9015, () => {
-	console.log("Server is running on port 9015");
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
