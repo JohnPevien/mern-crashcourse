b98b3cadb6a45aa02df587b937af16cd7613497a
John Pevien Laguindam <johnpevien.dev@gmail.com>
2025-05-14 23:35:46 +0800

    Set up routing and theme provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
+import { BrowserRouter } from "react-router";
+import { Theme } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Provider>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</StrictMode>
+);
